2ad6a4a5a8c25a4786316fb85672c3d7102e5d4d
Alej Kun <alejkun@gmail.com>
2022-06-13 16:48:10 -0500

    add data to player view

--- apps/peacefall-stats/pages/boss/[id].tsx
+++ apps/peacefall-stats/pages/boss/[id].tsx
@@ -1,6 +1,6 @@
 //peacefall.xyz/api/rankings/totalOwned?address=${owner}
 
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
 import useSWR from 'swr';
 import Image from 'next/image';
 import {FighterT, formatFighterResponse} from '@peacefall-stats/peacefall/utils'
@@ -12,16 +12,16 @@ const fetcher = async (url) => {
   const fighters = data?.map(formatFighterResponse)
 
   if (!res.ok) {
-    const error = { message: res.statusText };
+    const error = {message: res.statusText};
     throw error;
   }
   return fighters as FighterT[];
 };
 
 const Boss = () => {
-  const { query } = useRouter();
+  const {query} = useRouter();
 
-  const { data: warriors, error } = useSWR(
+  const {data: warriors, error} = useSWR(
     () => query.id && `/api/address/${query?.id}`,
     fetcher
   );
@@ -39,19 +39,19 @@ const Boss = () => {
         display: 'grid',
         justifyItems: 'center',
         gridGap: '1rem',
-        gridTemplateColumns: 'repeat( auto-fit, minmax(250px, 1fr)',
+        gridTemplateColumns: 'repeat( auto-fit, minmax(250px, 1fr)'
       }}
     >
-      {warriors?.map(({ id, name, image, hp, fights, level, peace }) => (
+      {warriors?.map(({id, name, image, hp, fights, level, peace, syndicate}) => (
         <div key={id}>
           <Link href={`/fighter/${id}`}>
-            <h2 style={{ cursor: 'pointer', marginBottom: 8 }}>
+            <h2 style={{cursor: 'pointer', marginBottom: 8}}>
               {name}
             </h2>
           </Link>
 
           <Link href={`/fighter/${id}`}>
-            <Image src={image} width={260} height={260} style={{ cursor: 'pointer' }}/>
+            <Image src={image} width={260} height={260} style={{cursor: 'pointer'}}/>
           </Link>
           <h2>
             Level {level} {peace === 'Yes' ? `☮️` : `⚔️`}
@@ -59,7 +59,17 @@ const Boss = () => {
           <p>id: {id}</p>
           <p>hp: {hp}</p>
           <p> fights: {fights?.length}</p>
+
+          {fights.map(({self, victor}) => {
+            const ownAttack = self.attack || syndicate;
+            const ownDefaulted = !self.attack;
+            return (
+              <div style={{display: 'grid', alignItems: 'center', gridAutoFlow: 'column'}}>
+              </div>
+            )
+          })}
         </div>
+
       ))}
     </div>
   );
